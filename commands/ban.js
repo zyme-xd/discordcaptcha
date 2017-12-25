3f1a192d4dacba282452340338aab761cd6d4e7e
Timo <30553356+y21@users.noreply.github.com>
2017-12-26 00:14:03 +0100

    Member would get kicked instead of getting banned

--- commands/ban.js
+++ commands/ban.js
@@ -1,5 +1,5 @@
 module.exports = (message) => {
     if (message.member.hasPermission('ADMINISTRATOR')) {
-        message.guild.member(message.mentions.users.first()).kick();
+        message.guild.member(message.mentions.users.first()).ban();
     }
 };
