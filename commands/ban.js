9775c291b9b7eddb143960a086fe74e8584b476e
Timo <30553356+y21@users.noreply.github.com>
2017-12-27 22:22:37 +0100

    JSDoc

--- commands/ban.js
+++ commands/ban.js
@@ -1,3 +1,6 @@
+/**
+ * @param {string} message - The message object, needed to get mentioned users, check if member has ADMINISTRATOR permissions and convert the first mentioned user in a guildmember 
+**/
 module.exports = (message) => {
     try {
     if (message.member.hasPermission('ADMINISTRATOR')) {
