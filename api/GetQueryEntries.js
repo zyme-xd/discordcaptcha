a71a4f79e2c4398411ddc62976465f44503687ce
Timo <30553356+y21@users.noreply.github.com>
2017-12-29 22:19:23 +0100

    Added try-catch block
    To prevent crashes

--- api/GetQueryEntries.js
+++ api/GetQueryEntries.js
@@ -1,7 +1,8 @@
 /**
  * @param {module} fs - The npm module fs
  * @returns {object} query - Query entries as object.
+ * @returns {error} Error - The error
  */
-module.exports = function(fs){
-    return JSON.parse(fs.readFileSync("./src/Query.json", "utf8"))['query'];
-}
+module.exports.all = function(fs){
+    try { return JSON.parse(fs.readFileSync("./src/Query.json", "utf8"))['query']; } catch (e) { return e; }
+ }
