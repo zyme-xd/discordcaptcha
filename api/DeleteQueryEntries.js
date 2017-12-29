714f1faea346e37258121d18bcd9dfb4a9ece9f6
Timo <30553356+y21@users.noreply.github.com>
2017-12-29 22:21:57 +0100

    JSDoc

--- api/DeleteQueryEntries.js
+++ api/DeleteQueryEntries.js
@@ -1,3 +1,6 @@
+/*
+ * @param {module} fs - The npm module fs
+*/
 module.exports.all = function(fs){
     try { return fs.writeFileSync("./src/Query.json", JSON.stringify({"query":{}})); } catch(e) { return e; }
 }
