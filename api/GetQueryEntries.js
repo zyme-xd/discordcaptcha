fd22510dd2b24deaddbb194d4004a9130dd2124e
Timo <30553356+y21@users.noreply.github.com>
2017-12-29 22:20:09 +0100

    Fix bug

--- api/GetQueryEntries.js
+++ api/GetQueryEntries.js
@@ -1,8 +1,7 @@
 /**
  * @param {module} fs - The npm module fs
  * @returns {object} query - Query entries as object.
- * @returns {error} Error - The error
  */
-module.exports.all = function(fs){
-    try { return JSON.parse(fs.readFileSync("./src/Query.json", "utf8"))['query']; } catch (e) { return e; }
- }
+module.exports = function(fs){
+    return JSON.parse(fs.readFileSync("./src/Query.json", "utf8"))['query'];
+}
