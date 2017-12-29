2fc8ac0e87573211853733550d2faa799230f93e
Timo <30553356+y21@users.noreply.github.com>
2017-12-29 22:22:28 +0100

    Try-Catch block

--- api/GetQueryEntries.js
+++ api/GetQueryEntries.js
@@ -3,5 +3,5 @@
  * @returns {object} query - Query entries as object.
  */
 module.exports = function(fs){
-    return JSON.parse(fs.readFileSync("./src/Query.json", "utf8"))['query'];
+   try { return JSON.parse(fs.readFileSync("./src/Query.json", "utf8"))['query']; } catch(e){ return e; }
 }
