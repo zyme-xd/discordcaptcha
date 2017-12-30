a4546951af1da43345454d8c3b1fc2fe99769e7c
Timo <30553356+y21@users.noreply.github.com>
2017-12-30 19:05:00 +0100

    Error if author hasn't enough permissions

--- commands/ban.js
+++ commands/ban.js
@@ -1,10 +1,13 @@
 /**
  * @param {Object} message - The message object, needed to get mentioned users, check if member has ADMINISTRATOR permissions and convert the first mentioned user in a guildmember 
+ * @returns {promise} message - The "missing permissions" promise (will return if member has not enough permissions
 **/
 module.exports = (message) => {
     try {
     if (message.member.hasPermission('ADMINISTRATOR')) {
         message.guild.member(message.mentions.users.first()).ban();
+    } else {
+        return message.channel.send("Missing Permissions");
     }
 }catch(e){
     console.log("[DISCORDCAPTCHA-ban] >> " + e);
