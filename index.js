84259ab9d027835220fdc2021e7fc4f4155ba380
Timo <30553356+y21@users.noreply.github.com>
2017-12-31 19:39:50 +0100

    Module unused
    This module is unused.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require("fs");
-const webshot = require("webshot");
 const snekfetch = require("snekfetch");
 
 // Command Imports
