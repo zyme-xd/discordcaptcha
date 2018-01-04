3d5f45bda0a8d0afcb6205ecfed24bb6ce7319a1
Timo <30553356+y21@users.noreply.github.com>
2018-01-04 20:40:58 +0100

    Wasn't assigned to be a function

--- api/DeleteQueryEntries.js
+++ api/DeleteQueryEntries.js
@@ -1,6 +1,6 @@
 /*
  * @param {module} fs - The npm module fs
 */
-module.exports.all = function(fs){
+module.exports = function(fs){
     try { return fs.writeFileSync("./src/Query.json", JSON.stringify({"query":{}})); } catch(e) { return e; }
 }
