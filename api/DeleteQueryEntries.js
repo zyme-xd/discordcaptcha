b84aa21050e827e10ea04e5d2a388da2cabf0200
Timo <30553356+y21@users.noreply.github.com>
2018-02-05 21:47:33 +0100

    fs parameter is not necessary

--- api/DeleteQueryEntries.js
+++ api/DeleteQueryEntries.js
@@ -4,6 +4,6 @@ sql.open('./src/db.sqlite');
 /*
  * @param {module} fs - The npm module fs
 */
-module.exports = async function(fs) {
+module.exports = async function() {
     sql.run('delete from queries').catch(e => console.log("[DeleteQueryEntries] An error occured: " + e));
 }
