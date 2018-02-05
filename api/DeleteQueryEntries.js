c8f98ce79f7b85483bf8f4d9391f556af9f366cf
Timo <30553356+y21@users.noreply.github.com>
2018-02-05 21:51:14 +0100

    Invalid jsdoc

--- api/DeleteQueryEntries.js
+++ api/DeleteQueryEntries.js
@@ -1,9 +1,6 @@
 let sql = require("sqlite");
 sql.open('./src/db.sqlite');
 
-/*
- * @param {module} fs - The npm module fs
-*/
 module.exports = async function() {
     sql.run('delete from queries').catch(e => console.log("[DeleteQueryEntries] An error occured: " + e));
 }
