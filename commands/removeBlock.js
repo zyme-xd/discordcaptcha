9c1669605e448b6f9385a1a2e7b04646ec9f9494
Timo <30553356+y21@users.noreply.github.com>
2018-02-05 22:50:39 +0100

    removeBlock update

--- commands/removeBlock.js
+++ commands/removeBlock.js
@@ -1,31 +1,17 @@
+let sql = require("sqlite");
+sql.open('./src/db.sqlite');
+
 /**
  * @param {Object} message - The message object
- * @param {Module} fs - The npm module fs
- * @param {string} prefix - The prefix
  * @param {array} contrib - Users that are allowed to use that command
-**/
-module.exports = (message, fs, prefix, contrib) => {
+ **/
+module.exports = async (message, contrib) => {
     try {
-        let file = JSON.parse(fs.readFileSync("./src/config.json", "utf8"));
-        const args = message.content.slice(prefix.length).trim().split(/ +/g);
         if (contrib.includes(message.author.tag)) {
-            if (message.mentions.users.size != 0) {
-                if (file.blockedIDs[message.mentions.users.first().id]) {
-                    if (file.blockedIDs[message.mentions.users.first().id].blocked == "true") {
-                        file.blockedIDs[message.mentions.users.first().id].blocked = "false";
-                        fs.writeFileSync("./src/config.json", JSON.stringify(file));
-                        message.channel.send("Successfully removed the block for `" + message.mentions.users.first().id + "`.")
-                    }
-                } else {
-                    message.channel.send("ID is not blocked.");
-                }
-            } else {
-                if (file.blockedIDs[args[1]]) {
-                    if (file.blockedIDs[args[1]].blocked == "true") {
-                        file.blockedIDs[args[1]].blocked = "false";
-                        fs.writeFileSync("./src/config.json", JSON.stringify(file));
-                        message.channel.send("Successfully removed the block for `" + args[1] + "`.")
-                    }
+            if (message.mentions.users.size === 0) {
+                if (await sql.get('select * from blocked where id="' + message.content.split(" ")[1] + '"')) {
+                    sql.run('delete from blocked where id="' + message.content.split(" ")[1] + '"');
+                    message.channel.send("Removed `" + message.content.split(" ")[1] + "` from the blocked list.");
                 } else {
                     message.channel.send("ID is not blocked.");
                 }
@@ -34,6 +20,6 @@ module.exports = (message, fs, prefix, contrib) => {
             return message.channel.send("Missing Permissions");
         }
     } catch (e) {
-        console.log("[DISCORDCAPTCHA-removeBlock] >> " + e);
+        console.log("[DISCORDCAPTCHA-block] >> " + e);
     }
-};
+}
