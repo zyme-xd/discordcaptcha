6e40830f11a4225e995e43e2f27c669f549aef74
Timo <30553356+y21@users.noreply.github.com>
2018-02-05 22:19:55 +0100

    Synchronous instead
    Making the commandhandler asynchronous instead of to resolve the promise.

--- api/GetLogs.js
+++ api/GetLogs.js
@@ -4,7 +4,7 @@ sql.open('./src/db.sqlite');
 /**
  * @returns {object} logs - Logs object
  */
-module.exports = async () => {
-    let logs = await sql.all('select * from logs').catch(e => console.log("An error occured: " + e));
+module.exports = () => {
+    let logs = sql.all('select * from logs').catch(e => console.log("An error occured: " + e));
     return logs;
 }
