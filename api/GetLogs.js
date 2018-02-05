d16a6d5997c1fbbe5d27bca53cd7e0894938f4e4
Timo <30553356+y21@users.noreply.github.com>
2018-02-05 22:14:06 +0100

    SQL instead of JSON

--- api/GetLogs.js
+++ api/GetLogs.js
@@ -1,7 +1,10 @@
+let sql = require("sqlite");
+sql.open('./src/db.sqlite');
+
 /**
- * @param {module} fs - The fs module
  * @returns {object} logs - Logs object
  */
-module.exports = fs => {
-    try { return JSON.parse(fs.readFileSync("./src/logs.json", "utf8")); } catch(e){ return e; }
+module.exports = async () => {
+    let logs = await sql.all('select * from logs').catch(e => console.log("An error occured: " + e));
+    return logs;
 }
