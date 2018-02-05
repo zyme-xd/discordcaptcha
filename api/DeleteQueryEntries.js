48da2f15065da3f18eff9baa2693df4c9dad0e38
Timo <30553356+y21@users.noreply.github.com>
2018-02-05 21:41:41 +0100

    SQL instead of JSON and asynchronous function

--- api/DeleteQueryEntries.js
+++ api/DeleteQueryEntries.js
@@ -1,6 +1,9 @@
+let sql = require("sqlite");
+sql.open('./src/db.sqlite');
+
 /*
  * @param {module} fs - The npm module fs
 */
-module.exports = function(fs){
-    try { return fs.writeFileSync("./src/Query.json", JSON.stringify({"query":{}})); } catch(e) { return e; }
+module.exports = async function(fs) {
+    sql.run('delete from queries').catch(e => console.log("[DeleteQueryEntries] An error occured: " + e));
 }
