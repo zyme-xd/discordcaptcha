9de7b36999e3d0d1bba7345cb46a9588b2eb2c99
Timo <30553356+y21@users.noreply.github.com>
2018-02-06 16:23:31 +0100

    Removed JSON stuff

--- commands/unverify.js
+++ commands/unverify.js
@@ -1,4 +1,5 @@
-const fs = require("fs");
+let sql = require("sqlite");
+sql.open('./src/db.sqlite');
 
 /**
  * @param {object} message - The message object
@@ -7,8 +8,5 @@ const fs = require("fs");
 
 module.exports = (message, config) => {
 	message.member.removeRole(config.userrole).catch(e => console.log(e));
-	let tempQueryFile = JSON.parse(fs.readFileSync("./src/Query.json", "utf8"));
-	tempQueryFile.query[message.author.id].verified = "false";
-	fs.writeFile("./src/Query.json", JSON.stringify(tempQueryFile), e => e ? console.log(e) : null);
 	message.reply("unverified!");
 };
