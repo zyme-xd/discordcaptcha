e42af006fa303f1c0fdfdabeb1ef2e5ec5d3a199
Timo <30553356+y21@users.noreply.github.com>
2018-04-13 20:08:02 +0200

    [Project#1] Remove GetCaptchas command

--- api/GetCaptchas.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-
-/**
- * @returns {object} files - An array with filenames
- */
-module.exports = () => {
-    try { return fs.readdirSync("./captchas") } catch(e){ return e; }
-}
