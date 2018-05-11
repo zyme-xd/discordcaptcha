9fe7265445a30733382134b7d97c0fc53f0bb480
Timo <30553356+y21@users.noreply.github.com>
2018-05-12 00:22:26 +0200

    ALWAYS PUSHING TO WRONG BRANCH! AAAA

--- src/Commands.js
+++ src/Commands.js
@@ -1,84 +1,36 @@
 module.exports = async (message, config, Discord, fs, latestVersion) => {
-	var blockCommand, removeBlockCommand, banCommand, clearCommand, verifylogs, versionCommand, createRole;
-	config["commands"]["blockUser"].enabled ? blockCommand = require("./commands/block.js") : blockCommand = false;
-	config["commands"]["removeBlockFromUser"].enabled ? removeBlockCommand = require("./commands/removeBlock.js") : removeBlockCommand = false;
-	config["commands"]["banGuildMember"].enabled ? banCommand = require("./commands/removeBlock.js") : banCommand = false;
-	config["commands"]["clear"].enabled ? clearCommand = require("./commands/clear.js") : clearCommand = false;
-	config["commands"]["version"].enabled ? versionCommand = true : versionCommand = false;
-	config["commands"]["makerole"].enabled ? createRole = require("./commands/makerole.js") : createRole = false;
-	const Handler = require("../api/Handler.js");
+	let enabledCommands = [ ];
 
-	// Bot Commands
-	if (message.content.startsWith(config.prefix)) {
-		switch (message.content.split(" ")[0]) {
-		case config.prefix + config["commands"]["banGuildMember"].command:
-			if (!banCommand) return;
-			banCommand(message, config["commands"]["banGuildMember"].contributors)
-			break;
-		case config.prefix + config["commands"]["blockUser"].command:
-			if (!blockCommand) return;
-			blockCommand(message, config["commands"]["blockUser"].contributors);
-			break;
-		case config.prefix + config["commands"]["removeBlockFromUser"].command:
-			if (!removeBlockCommand) return;
-			removeBlockCommand(message, config["commands"]["removeBlockFromUser"].contributors);
-			break;
-		case config.prefix + config["commands"]["clear"].command:
-			if (!clearCommand) return;
-			clearCommand(message, config["commands"]["clear"].contributors);
-			break;
-		case config.prefix + "eval":
-			if (message.author.id === config.ownerid && config.evalAllowed === "true") {
-				message.channel.send(":outbox_tray: Output: ```JavaScript\n" + eval(message.content.substr(6)) + "\n```");
-			}
-			break;
-		case config.prefix + config["commands"]["version"].command:
-			versionCommand ? message.channel.send(new Discord.RichEmbed()
-				.setColor("RANDOM")
-				.setTitle("Version")
-				.setDescription(`Current DiscordCaptcha version: \`${config.version}\`\nLatest version: \`${latestVersion}\``)
-				.addField("Repository", "https://github.com/y21/discordcaptcha/")
-				.setTimestamp()
-			) : null;
-			break;
-		case config.prefix + config["commands"]["makerole"].command:
-			if (!createRole) return;
-			createRole(message);
-			break;
-		case config.prefix + config["commands"]["unverify"].command:
-			if (config["commands"]["unverify"].enabled && message.member.roles.has(config.userrole)) {
-				require("../commands/unverify.js")(message, config);
-			}
-			break;
-		}
+	for(const command of Object.keys(config.commands)){
+		if(config.commands[command].enabled) enabledCommands.push(config.commands[command]);
 	}
 
+	if(config.evalAllowed) enabledCommands.push({ command: "eval" });
 
-	// API Commands
-	if (message.content.startsWith(config.prefix)) {
-			switch (message.content.split(" ")[0].substr(config.prefix.length)) {
-			case config["commands"]["queries"].command:
-				if (message.author.id === config.ownerid && config["commands"]["queries"].enabled) {
-					message.channel.send("```js\n// Query\n\n" + require("util").inspect(await new Handler("GetQueryEntries").request()) + "\n```");
-				}
-				break;
-			case config["commands"]["querydelete"].command:
-				if (config["commands"]["querydelete"].contributors.includes(message.author.tag) && config["commands"]["querydelete"].enabled) {
-					new Handler("DeleteQueryEntries").request();
-					message.channel.send("Deleted the query.");
-				}
-				break;
-			case config["commands"]["purgelogs"].command:
-				if (config["commands"]["purgelogs"].contributors.includes(message.author.tag) && config["commands"]["purgelogs"].enabled) {
-					new Handler("PurgeVerifyLogs").request();
-					message.channel.send("Purged logs.");
-				}
-				break;
-			case config["commands"]["logs"].command:
-				if (config["commands"]["logs"].contributors.includes(message.author.tag) && config["commands"]["logs"].enabled) {
-					message.channel.send("```js\n// Logs\n\n" + require("util").inspect(await new Handler("GetLogs").request()) + "\n```");
-				}
-				break;
-			}
+	const Handler = require("../api/Handler.js");
+
+	let [command, ...args] = message.content.split(" ");
+	command = command.substr(config.prefix.length);
+	if(enabledCommands.find(e => e.command === command)){
+		if(command === config.commands.blockUser.command) require("./commands/block.js")(message, config.commands.blockUser.contributors);
+		if(command === config.commands.banGuildMember.command) require("./commands/ban.js")(message, config.commands.banGuildMember.contributors);
+		if(command === config.commands.removeBlockFromUser.command) require("./commands/removeBlock.js")(message, config.commands.removeBlockFromUser.contributors);
+		if(command === config.commands.clear.command) require("./commands/clear.js")(message, config.commands.clear.contributors);
+		if(command === "eval" && message.author.id === config.ownerid && config.evalAllowed) message.channel.send(":outbox_tray: Output: ```JavaScript\n" + eval(message.content.substr(6)) + "\n```");
+		if(command === config.commands.version.command) message.channel.send(new Discord.RichEmbed().setColor("RANDOM").setTitle("Version").setDescription(`Current DiscordCaptcha version: \`${config.version}\`\nLatest version: \`${latestVersion}\``).addField("Repository", "https://github.com/y21/discordcaptcha/").setTimestamp());
+		if(command === config.commands.makerole.command) require("./commands/makerole.js")(message, config.commands.makerole.contributors);
+		if(command === config.commands.unverify.command) require("../commands/unverify.js")(message, config);
+
+		// Utility (aka API) commands
+		if(command === config.commands.queries.command && config.commands.queries.contributors.includes(message.author.tag)) message.channel.send("```js\n// Query\n\n" + require("util").inspect(await new Handler("GetQueryEntries").request()) + "\n```");
+		if(command === config.commands.querydelete.command && config.commands.querydelete.contributors.includes(message.author.tag)) {
+			new Handler("DeleteQueryEntries").request();
+			message.channel.send("Deleted the query.");
+		}
+		if(command === config.commands.purgelogs.command && config.commands.purgelogs.contributors.includes(message.author.tag)) {
+			new Handler("PurgeVerifyLogs").request();
+			message.channel.send("Deleted the query.");
+		}
+		if(command === config.commands.logs.command && config.commands.logs.contributors.includes(message.author.tag)) message.channel.send("```js\n// Logs\n\n" + require("util").inspect(await new Handler("GetLogs").request()) + "\n```");
 	}
 };
