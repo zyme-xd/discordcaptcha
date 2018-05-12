324a45379a6647d98ab8c615c8f09a99ac796c2d
Timo <30553356+y21@users.noreply.github.com>
2018-05-12 18:55:04 +0200

    quotes + semi fixes (Codacy) + arrow function

--- api/PurgeVerifyLogs.js
+++ api/PurgeVerifyLogs.js
@@ -1,6 +1,6 @@
 let sql = require("sqlite");
-sql.open('./src/db.sqlite');
+sql.open("./src/db.sqlite");
 
-module.exports = function() {
-    sql.run('delete from logs').catch(e => console.log("[PurgeVerifyLogs] An error occured: " + e));
-}
+module.exports = () => {
+    sql.run("delete from logs").catch(e => console.log("[PurgeVerifyLogs] An error occured: " + e));
+};
