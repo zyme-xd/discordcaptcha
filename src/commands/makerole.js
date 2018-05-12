6bec95e68a9c90f3e7ac94d2c2a3c41a0f2b9cbc
Timo <30553356+y21@users.noreply.github.com>
2018-05-12 18:57:30 +0200

    eqeqeq fixes (Codacy)

--- src/commands/makerole.js
+++ src/commands/makerole.js
@@ -8,7 +8,7 @@ module.exports = (message) => {
     if(message.guild.roles.get(config.userrole)){
         message.reply("There's already a role for verifications. React with 🇾 if you want me to create one or with 🇳 to cancel.")
         .then(m => {m.react("🇾"); m.react("🇳"); m.awaitReactions(
-        mes => (mes.emoji.name === "🇾" && mes.users.filter(u => u.id === message.author.id)).size == 1 ? (() => { message.guild.createRole({name:"Users",permissions:["SEND_MESSAGES"]}); message.reply("`Users` role created!"); })() : null
+        mes => (mes.emoji.name === "🇾" && mes.users.filter(u => u.id === message.author.id)).size === 1 ? (() => { message.guild.createRole({name:"Users",permissions:["SEND_MESSAGES"]}); message.reply("`Users` role created!"); })() : null
         )});
     } else {
         message.guild.createRole({name:"Users",permissions:["SEND_MESSAGES"]});
