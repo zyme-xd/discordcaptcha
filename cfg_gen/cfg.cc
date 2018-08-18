814370c8590d2486b27c2dbd73fe073d5daff0a2
Timo <30553356+y21@users.noreply.github.com>
2018-08-18 23:28:46 +0200

    Add commands

--- cfg_gen/cfg.cc
+++ cfg_gen/cfg.cc
@@ -5,6 +5,22 @@
 #include <fstream>
 #include "cfg.h"
 
+const std::string cfg_input::version = "3.0.1";
+std::vector<std::string> commands {
+	"blockUser",
+	"removeBlockFromUser",
+	"banGuildMember",
+	"clear",
+	"version",
+	"queries",
+	"querydelete",
+	"purgelogs",
+	"logs",
+	"captchas",
+	"makerole",
+	"unverify"
+};
+
 std::map<std::string, std::string> translations::en = {
     { "MENU_RUN_CFG", "Run config file generator" },
     { "MENU_CREDITS", "Credits" },
@@ -25,6 +41,8 @@ std::map<std::string, std::string> translations::en = {
     { "C_CMDSTATUS", "Enable or disable the following command (Answer with y or n): {CMD}: " },
     { "C_AUTOADD", "Automatically allow you to execute all commands? (Answer with y or n): " }
 };
+
+std::vector<std::string> cfg_dcmd_dec::cmdnames = commands;
 
 std::string* cfg_util::dstream::getGameName() const
 {
@@ -79,6 +97,7 @@ void cfg_util::dcommand::setContributors(std::vector<std::string>& contributors)
 std::string cfg_util::dcommand::addContributor(std::string new_contrib)
 {
 	(this->contributors)->push_back(new_contrib);
+	return new_contrib;
 }
 
 bool cfg_util::dcommand::isEnabled() const
@@ -115,10 +134,10 @@ int main()
         case 1:
             if(lang == translations::LANG::EN)
             {
-                file.close();
                 std::cout << translations::en["C_BOT_TOKEN"];
-                std::cin >> cfg_input::token;
-
+				std::cin.ignore();
+                getline(std::cin, cfg_input::token);
+				
                 std::cout << translations::en["C_CLIENT_ID"];
                 std::cin >> cfg_input::clientid;
 
@@ -129,36 +148,81 @@ int main()
                 std::cin >> cfg_input::logChannel;
 
                 std::cout << translations::en["C_VERIFIEDROLE"];
-                std::cin >> cfg_input::logChannel;
+                std::cin >> cfg_input::verifiedRole;
 
                 std::string strname, strurl;
                 std::cout << translations::en["C_STREAMNAME"];
-                std::cin >> strname;
+                std::cin.ignore();
+                getline(std::cin, strname);
 
                 std::cout << translations::en["C_STREAMURL"];
                 std::cin >> strurl;
 
-		stream->setGameName(strname);
-		stream->setStreamURL(strurl);
+				stream->setGameName(strname);
+				stream->setStreamURL(strurl);
 				
-		std::string* logging = new std::string;
+				std::string* evalALWDs = new std::string;
                 std::cout << translations::en["C_EVALALWD"];
-                std::cin >> *logging;
-                cfg_input::eval = *logging == "y";
-                delete logging;
+                std::cin >> *evalALWDs;
+                cfg_input::eval = *evalALWDs == "y";
+                delete evalALWDs;
 
                 std::cout << translations::en["C_OWNER"];
-                std::cin >> cfg_input::logChannel;
+                std::cin >> cfg_input::owner;
 
                 std::cout << translations::en["C_OWNERTAG"];
-                std::cin >> cfg_input::logChannel;
+                std::cin.ignore();
+                getline(std::cin, cfg_input::ownerTag);
 
+				std::string* logging = new std::string;
                 std::cout << translations::en["C_LOGTODB"];
-                std::cin >> cfg_input::logChannel;
+                std::cin >> *logging;
+                cfg_input::logToDB = *logging == "y";
+                delete logging;
 
+				std::string* ctype = new std::string;
                 std::cout << translations::en["C_CAPTCHATYPE"];
-                std::cin >> cfg_input::logChannel;
+                std::cin >> *ctype;
+                cfg_input::ctype = (*ctype == "IMAGES") ? cfg_util::captchaType::IMAGE : cfg_util::captchaType::TEXT;
+                delete ctype;
                 
+                file << "{" << std::endl
+                << "\t\"token\": \"" << cfg_input::token << "\"," << std::endl
+                << "\t\"clientid\": \"" << cfg_input::clientid << "\"," << std::endl
+				<< "\t\"prefix\": \"" << cfg_input::prefix << "\"," << std::endl
+				<< "\t\"version\": \"" << cfg_input::version << "\"," << std::endl
+				<< "\t\"chat\": \"" << cfg_input::logChannel << "\"," << std::endl
+				<< "\t\"userrole\": \"" << cfg_input::verifiedRole << "\"," << std::endl
+				<< "\t\"streamingGame\": \"" << stream->getGameName() << "\"," << std::endl
+				<< "\t\"streamingLink\": \"" << stream->getStreamURL() << "\"," << std::endl
+				<< "\t\"evalAllowed\": \"" << cfg_input::eval << "\"," << std::endl
+				<< "\t\"captchaType\": \"" << (cfg_input::ctype == cfg_util::captchaType::IMAGE ? "image" : "text") << "\"," << std::endl
+				<< "\t\"commands\": {" << std::endl;
+					
+				std::cout << "------------------------------" << std::endl
+				<< "Commands" << std::endl
+				<< "------------------------------" << std::endl;
+				int index = 0;
+                for(std::string& cmd : cfg_dcmd_dec::cmdnames)
+                {
+                	std::string* input = new std::string{""};
+                	std::cout << "Command '" << cmd << "' | Alias (leave blank if it should stay): ";
+                	std::cin >> *input;
+                	cfg_dcmd_dec::execnames.push_back(*input == "" ? cmd : *input);
+                	std::cout << "Command '" << cmd << "' | Automatically allow you to use this command? (Answer with y or n): ";
+                	std::cin.ignore();
+					getline(std::cin, *input);
+                	if (*input == "y") cfg_dcmd_dec::allowSelf.push_back(true);
+                	std::cout << "Command '" << cmd << "' | Enable this command? (Answer with y or n): ";
+                	std::cin >> *input;
+                	cfg_dcmd_dec::cmdstatuses.push_back(*input == "y");
+                	file << "\t\t\"" << commands.at(index) << "\": {" << std::endl
+                	<< "\t\t\t\"command\": \"" << cfg_dcmd_dec::execnames.at(index) << "\"," << std::endl
+                	<< "\t\t\t\"contributors\": [ \"" << (cfg_dcmd_dec::allowSelf.at(index) ? cfg_input::ownerTag : "") << "\" ]" << std::endl;
+                	
+                	delete input;
+                	index++;
+				}
                 file.close();
             }
             break;
@@ -166,6 +230,7 @@ int main()
             return 0;
             break;
     }
+    
     delete stream;
     return 0;
 }
