23e9ccd916be68ce4647162bd8fa35de695d9eed
Timo <30553356+y21@users.noreply.github.com>
2018-08-19 15:50:19 +0200

    Format this as well

--- cfg_gen/cfg.h
+++ cfg_gen/cfg.h
@@ -3,73 +3,73 @@
 
 namespace translations
 {
-    enum LANG { DE, EN };
-    extern std::map<std::string, std::string> en;
+enum LANG { DE, EN };
+extern std::map<std::string, std::string> en;
 }
 
 namespace cfg_util
 {
-    enum captchaType { IMAGE, TEXT };
-    class dstream
-    {
-        private:
-	std::string* gameName;
-	std::string* streamURL;
-        public:
-	std::string* getGameName() const;
-	void setGameName(std::string&);
-	std::string* getStreamURL() const;
-	void setStreamURL(std::string&);
-    };
+enum captchaType { IMAGE, TEXT };
+class dstream
+{
+private:
+    std::string* gameName;
+    std::string* streamURL;
+public:
+    std::string* getGameName() const;
+    void setGameName(std::string&);
+    std::string* getStreamURL() const;
+    void setStreamURL(std::string&);
+};
 
-    class dcommand
-    {
-        private:
-	std::string* commandName;
-	std::string* execName;
-	std::vector<std::string>* contributors;
-	bool* enabled;
-        public:
-	std::string* getCommandName() const;
-	void setCommandName(std::string&);
-	std::string* getExecutionName() const;
-	void setExecutionName(std::string&);
-	std::vector<std::string>* getContributors() const;
-	void setContributors(std::vector<std::string>&);
-	std::string addContributor(std::string);
-	bool isEnabled() const;
-	void setStatus(bool&);
-    };
+class dcommand
+{
+private:
+    std::string* commandName;
+    std::string* execName;
+    std::vector<std::string>* contributors;
+    bool* enabled;
+public:
+    std::string* getCommandName() const;
+    void setCommandName(std::string&);
+    std::string* getExecutionName() const;
+    void setExecutionName(std::string&);
+    std::vector<std::string>* getContributors() const;
+    void setContributors(std::vector<std::string>&);
+    std::string addContributor(std::string);
+    bool isEnabled() const;
+    void setStatus(bool&);
+};
 }
 
 namespace cfg_input
 {
-	extern const std::string version;
-	std::string token,
-	clientid,
-	prefix,
-	logChannel,
-	verifiedRole,
-	owner,
-	ownerTag;
+extern const std::string version;
+std::string token,
+    clientid,
+    prefix,
+    logChannel,
+    verifiedRole,
+    owner,
+    ownerTag;
 
-	cfg_util::dstream stream;
+cfg_util::dstream stream;
 
-    bool eval,
-    logToDB;
+bool eval,
+     logToDB;
 
-    cfg_util::captchaType ctype;
+cfg_util::captchaType ctype;
 
-    std::vector<cfg_util::dcommand> commands;
+std::vector<cfg_util::dcommand> commands;
 }
 
 namespace cfg_dcmd_dec
 {
-	std::vector<cfg_util::dcommand> dcmds;
-    	extern std::vector<std::string> cmdnames;
-	std::vector<std::string> execnames;
-	std::vector<bool> allowSelf;
-	std::vector<bool> cmdstatuses;
+std::vector<cfg_util::dcommand> dcmds;
+extern std::vector<std::string> cmdnames;
+std::vector<std::string> execnames;
+std::vector<bool> allowSelf;
+std::vector<bool> cmdstatuses;
 }
 
 #endif
