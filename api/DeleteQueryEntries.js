464943300a312bb6df919bb2f84ff240a8706c81
Timo <artur.fiedler54@gmail.com>
2019-01-03 20:24:37 +0100

    Remove this trash

--- api/DeleteQueryEntries.js
+++
@@ -1,6 +0,0 @@
-let sql = require("sqlite");
-sql.open("./src/db.sqlite");
-
-module.exports = async function() {
-    sql.run("delete from queries").catch(e => console.log("[DeleteQueryEntries] An error occured: " + e));
-};
