18602554c90eaaa31802eb5dd4336ad789956446
Timo <artur.fiedler54@gmail.com>
2019-01-03 20:07:49 +0100

    Pass config to command

--- src/Commandhandler.js
+++ src/Commandhandler.js
@@ -10,6 +10,6 @@ module.exports = class CommandHandler {
         if (!this.commands.some(v => v.alias === message.command)) return;
         if (message.author.id !== this.config.ownerid && !Object.entries(this.config.commands).find(v => v[1].command === message.command)[1].contributors.includes(message.author.tag)) return;
         if (this.commands.find(v => v.alias === message.command).enabled !== true) return message.reply("❌ | Command disabled.");
-        this.commands.find(v => v.alias === message.command).run(message, this.sql);
+        this.commands.find(v => v.alias === message.command).run(message, this.sql, this.config);
     }
 }
