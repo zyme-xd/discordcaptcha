5412c0dcf1572aa9e808c745aea12a70f6471f1b
Timo <artur.fiedler54@gmail.com>
2019-01-03 03:19:54 +0100

    functional commandhandler

--- src/Commandhandler.js
+++ src/Commandhandler.js
@@ -1,5 +1,12 @@
 module.exports = class CommandHandler {
-    constructor() {
-        
+    constructor(commands, config) {
+        this.config = config;
+        this.commands = commands;
+    }
+    exec(message) {
+        message.command = message.content.split(" ")[0].substr(this.config.prefix.length);
+        if (!this.commands.some(v => v.alias === message.command)) return;
+        if (message.author.id !== this.config.ownerid && !Object.entries(this.config.commands).find(v => v[1].command === message.command)[1].contributors.includes(message.author.tag)) return;
+        this.commands.find(v => v.alias === message.command).run(message);
     }
 }
