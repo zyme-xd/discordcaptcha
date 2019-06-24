aea6d15cbd74929a9b239580d341577ba78da283
Timo <artur.fiedler54@gmail.com>
2019-06-24 20:43:04 +0200

    support presence

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,16 @@ const versionCheck = require("../utils/versionCheck");
 module.exports.run = async function() {
     console.log(`[LOG] Logged in (${this.guilds.size} servers - ${this.users.size} users)!`);
 
+    if (Object.keys(this.config.presence).length !== 0) {
+        this.user.setPresence({
+            game: {
+                name: this.config.presence.name,
+                type: this.config.presence.type
+            },
+            status: "online"
+        }).catch(console.error);
+    }
+
     // Check version
     await versionCheck();
 
