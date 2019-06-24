ce214509a04c8d968b88cc991d7de3bd821094af
Timo <artur.fiedler54@gmail.com>
2019-06-24 19:40:37 +0200

    fix command handler

--- events/message.js
+++ events/message.js
@@ -1,6 +1,6 @@
 module.exports.run = function(message) {
     // Check if message starts with prefix, is not sent by a bot and not in DMs
-    if (message.content.startsWith(this.config.prefix) || !message.author.bot || message.guild) return;
+    if (!message.content.startsWith(this.config.prefix) || message.author.bot || !message.guild) return;
 
     // Define required properties
     Object.defineProperties(message, {
@@ -13,8 +13,24 @@ module.exports.run = function(message) {
     });
 
     // Check if command exists
-    if (!this.commands.has(message.command)) return;
+    const command = this.commands.get(message.command);
+    const configCommand = this.config.commands[message.command];
+    if (!command) return;
+
+    // Check if command is enabled
+    if (command.enabled === false) return message.reply("⛔ | This command has been disabled.");
+
+    // Check if author is allowed to execute command
+    if (configCommand.executors.length > 0 && !configCommand.executors.includes(message.author.id))
+        return message.reply("⛔ | You are not allowed to execute this command.");
+    if (configCommand.requiredPermissions.length > 0 && !configCommand.requiredPermissions.some(v => message.member.hasPermission(v.toUpperCase())))
+        return message.reply("⛔ | You are not allowed to execute this command.");
+
+    // Check args length
+    const requiredArgs = command.info.args.filter(v => v.required).length;
+    if (message.args.length !== requiredArgs)
+        return message.reply(`⛔ | Invalid arguments: ${requiredArgs.length} are needed but only ${message.command.length} were provided.`);
 
     // Run command
-    this.commands.get(message.command).bind(this)(message);
+    command.call(this, message);
 };
