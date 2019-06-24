3317ba9c0cfa9c64a4dca0e65cba5678f1bc3fa3
Timo <artur.fiedler54@gmail.com>
2019-06-24 19:11:24 +0200

    event stuff

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,12 @@
-module.exports.run = function() {
-    console.log("Logged in!");
+const versionCheck = require("../utils/versionCheck");
+
+module.exports.run = async function() {
+    console.log(`[LOG] Logged in (${this.guilds.size} servers - ${this.users.size} users)!`);
+
+    // Check version
+    await versionCheck();
+
+    // Init DB
+    await this.db.open("./database.sqlite");
+    await this.db.run("CREATE TABLE IF NOT EXISTS `queries` (`id` TEXT)");
 };
