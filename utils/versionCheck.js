396c4fda8f1023a9f4f0c99999903c87468ee312
Timo <artur.fiedler54@gmail.com>
2019-06-24 21:46:45 +0200

    return Promise<boolean> instead of logging

--- utils/versionCheck.js
+++ utils/versionCheck.js
@@ -4,7 +4,6 @@ const pkg = require("../package");
 module.exports = async () => {
     const request = await fetch("https://raw.githubusercontent.com/y21/discordcaptcha/master/package.json");
     const json = await request.json();
-    if (pkg.version !== json.version) {
-        console.log("[WARN] Latest version is not identical to local version");
-    }
+
+    return pkg.version === json.version;
 };
