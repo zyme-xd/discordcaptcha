a6725dd9301de7af3ca721025afe91049829f666
Timo <artur.fiedler54@gmail.com>
2019-06-24 20:30:46 +0200

    add verify command

---
+++ commands/verify.js
@@ -0,0 +1,60 @@
+const { randomBytes } = require("crypto");
+const Jimp = require("jimp");
+const { Attachment, RichEmbed } = require("discord.js");
+
+module.exports = async function(message) {
+
+    // Check if channel name is "verify" or in verificationChannels array
+    if (message.channel.id !== (this.config.servers[(message.guild || {id: "0"}).id] || {verificationChannel: "0"}).verificationChannel && message.channel.name !== "verify") return;
+
+    if (message.args.length === 0) {
+        // No arguments provided
+
+        const captcha = randomBytes(32).toString("hex").substr(0, 6);
+        const font = await Jimp.loadFont(Jimp.FONT_SANS_64_WHITE);
+        const image = await Jimp.read("./assets/captcha_bg.png");
+        image.resize(800, 800);
+        image.print(font, Math.floor(Math.random() * 600), Math.floor(Math.random() * 600), captcha);
+        const buffer = await image.getBufferAsync(Jimp.MIME_JPEG);
+        const embed = new RichEmbed()
+            .setTitle("Verification")
+            .setDescription("This server is protected by [DiscordCaptcha](https://github.com/y21/discordcaptcha), an open-source project that prevents servers from being raided.\n" +
+                "Please solve this captcha by sending `" + this.config.prefix + "verify [code]` in <#" + message.channel.id + ">")
+            .attachFile({ attachment: buffer, name: "captcha.jpeg" })
+            .setImage("attachment://captcha.jpeg");
+        message.author.send(embed).catch(() => {
+            message.reply("⛔ | Could not send captcha, maybe you have Direct Messages disabled?");
+        });
+
+        this.query.set(message.author.id, captcha);
+
+    } else {
+        // Arguments provided
+        if (!this.query.has(message.author.id)) return message.reply("⛔ | Please request a captcha by sending `" + this.config.prefix + "verify`");
+
+        const captcha = this.query.get(message.author.id);
+
+        if (message.args[0] !== captcha) return message.reply("⛔ | Invalid captcha!");
+        else {
+            message.member.addRole(this.config.servers[message.guild.id].verifyRole).then(() => {
+                message.reply("✅ | Successfully verified.");
+            }).catch(console.error);
+            this.query.delete(message.author.id);
+        }
+
+    }
+
+
+
+
+
+
+
+};
+
+module.exports.info = {
+    description: "",
+    args: [
+        { required: false, description: "The captcha code", name: "captcha" }
+    ]
+};
