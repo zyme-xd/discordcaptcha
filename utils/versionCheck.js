38c6de11c65f605aa6f5c1808b468d38c9c045ec
Timo <artur.fiedler54@gmail.com>
2019-06-24 19:10:28 +0200

    make function async

--- utils/versionCheck.js
+++ utils/versionCheck.js
@@ -1,12 +1,10 @@
 const fetch = require("node-fetch");
 const pkg = require("../package");
 
-module.exports = () => {
-    fetch("https://raw.githubusercontent.com/y21/discordcaptcha/master/package.json")
-        .then(v => v.json())
-        .then(v => {
-            if (pkg.version !== v.version) {
-                console.log("[WARN] Latest version is not identical to local version");
-            }
-        });
+module.exports = async () => {
+    const request = await fetch("https://raw.githubusercontent.com/y21/discordcaptcha/master/package.json");
+    const json = await request.json();
+    if (pkg.version !== json.version) {
+        console.log("[WARN] Latest version is not identical to local version");
+    }
 };
