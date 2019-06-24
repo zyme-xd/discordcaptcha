892407567fe4f07a2afeb73e611fb102a1e03361
Timo <artur.fiedler54@gmail.com>
2019-06-24 16:42:28 +0200

    remove src stuff

--- src/commands/queries.js
+++
@@ -1,7 +0,0 @@
-module.exports = class queriesCommand {
-    static run(message, sqlite) {
-        sqlite.all("SELECT * FROM queries").then(v => {
-            message.reply(v.map(val => val.id).join("\n"));
-        });
-    }
-}
