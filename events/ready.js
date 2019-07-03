86e68aba4f853a0f5f1bba27e1ab624523d16f9f
Timo <artur.fiedler54@gmail.com>
2019-07-03 19:22:08 +0200

    check if guild is present in config file

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,10 @@ const versionCheck = require("../utils/versionCheck");
 module.exports.run = async function() {
     console.log(`[LOG] Logged in (${this.guilds.size} servers - ${this.users.size} users)!`);
 
+    for (const guild of this.guilds.values()) {
+        if (!this.config.servers[guild.id]) console.log(`[WARN] ${guild.name} is not set in \`servers\` (config.json file)`);
+    }
+
     if (Object.keys(this.config.presence).length !== 0) {
         this.user.setPresence({
             game: {
